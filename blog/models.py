b541de5a61316021bd502a8aee8b919e7478e048
Jon Lee <39420293+4ka0@users.noreply.github.com>
2020-03-01 13:32:00 +0000

    Initial commit

---
+++ blog/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+class Post(models.Model):
+    ''' 
+    Class representing a single blog post.
+    
+    For the author we're using a ForeignKey field as this allows for 
+    a many-to-one relationship, meaning that the author can be the author
+    of many blog posts.
+    
+    The User model is referenced to make authentication possible.
+    
+    Many-to-one relationships require an on_delete option. Here CASCADE is
+    used, meaning that when the referenced object is deleted, associated 
+    objects are also deleted (i.e. when you remove a blog post, the associated 
+    comments are also deleted).
+    '''
+    
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.title
